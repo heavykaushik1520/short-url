02086d22c36c26cf542f02a44de521881a230855
Kaushik <kaushikmandavkar6@gmail.com>
2024-04-08 19:09:01 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 8001 ;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(()=>
+console.log("Connected to MongoDB"));
+
+app.use(express.json());
+
+app.get("/:shortId", async (req , res)=>
+{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId ,
+        },
+        {
+            $push:{
+                timestamp : Date.now(),
+            }
+        }
+    )
+    res.redirect(entry.redirectURL);
+})
+
+app.listen(PORT , ()=>console.log("Server Listening"));
